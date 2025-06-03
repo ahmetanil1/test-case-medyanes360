c4dda0a8aec04832e66b277e0116d0e3fbfeca17
ahmetanil1 <amtyrgnc@gmail.com>
2025-06-03 23:16:12 +0300

    Add method tests

---
+++ pages/api/test/index.jsx
@@ -0,0 +1,13 @@
+const handler = async (req, res) => {
+
+    if(req.method === "POST") {
+        return res.status(200).json({message: "Method POST"})
+    }
+    if(req.method === "GET") {
+
+        return res.status(200).json({message: "Method GET"})
+    }
+
+}
+
+export default handler;
