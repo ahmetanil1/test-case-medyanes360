bf677871f3b00ffe4211d9958cc500cf5a09c0f8
ahmetanil1 <amtyrgnc@gmail.com>
2025-06-05 08:23:34 +0300

    Add user session for login user

---
+++ app/layout.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+
+
+
+export default function RootLayout({ children, session }) {
+  return (
+    <html lang="tr">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+          integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
+      </head>
+      <body>
+        <SessionProvider session={session}>
+          {children}
+        </SessionProvider>
+      </body>
+    </html>
+  );
+}
